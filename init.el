5e8f7a841f1398524bf09c3dc052698d89f10f0e
Carlos Latugaye <carlos@code54.com>
2015-02-19 08:02:20 -0300

    Disable toolbar

--- init.el
+++ init.el
@@ -3,3 +3,7 @@
 (add-to-list 'package-archives
              '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
 (package-initialize)
+
+
+;; General Configuration
+(tool-bar-mode -1)
