301f2be57f80414766fe529da9d67342121e6707
Carlos Latugaye <carlos@code54.com>
2016-01-06 11:44:12 -0300

    Add JS configuration

--- init.el
+++ init.el
@@ -31,3 +31,11 @@
                            :weight 'light)
                  nil
                  t)
+
+;; Javascript
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js-mode-hook 'js2-minor-mode)
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+
+(custom-set-variables
+ '(js2-basic-offset 2))
